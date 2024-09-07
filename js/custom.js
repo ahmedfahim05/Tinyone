7da9c128908d90df2755b6f36776ab73c288e90f
ahmedfahim05 <you@example.comfahim,ahmed1064@gmail.com>
2024-09-07 23:54:37 +0600

    first commit

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){    
+    $('#menubtn').click(function(){
+        $('.navbar-nav').slideToggle();
+    })
+
+    $('#menubtn').click(function(){
+        $('#inspire').css("margin-top", "150px");
+      });
+
+}); 
+
